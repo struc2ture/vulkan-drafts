31a933910e6455a6d09ec5f6ff533f20f5815957
struc2ture <struc0ture@gmail.com>
2025-08-21 14:56:14 -0700

    1 window

---
+++ 1/README.md
@@ -0,0 +1,2 @@
+- CPP Hello world
+- GLFW window
--- 1/src/main.cpp
+++ 1/src/main.cpp
@@ -1,6 +1,22 @@
 #include <cstdio>
 
+#include <GLFW/glfw3.h>
+
 int main()
 {
-    printf("Hello world\n");
+    int width = 1000;
+    int height = 900;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow *window = glfwCreateWindow(width, height, "Vulkan", NULL, NULL);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
 }
